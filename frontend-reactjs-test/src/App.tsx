a31a406e42b04db16872f07c09cae055ff6bdd05
Hung Nguyen Huy <hungnh1812@gmail.com>
2020-10-02 01:27:18 +0700

    - create top sticky menu bar

--- frontend-reactjs-test/src/App.tsx
+++ frontend-reactjs-test/src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import './css/topbar.css';
+import logo from './data/logo1.png'
+import { Button, Form, FormControl, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
+    <>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -15,11 +20,36 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
+        >ß
           Learn React
         </a>
-      </header>
-    </div>
+      </header> */}
+      <nav className="navbar static-top navbar-default">
+        <div className="container">
+          <img src={logo} alt="logo"/>
+        </div>
+        {/* <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand> */}
+        {/* <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse> */}
+      </nav>
+    </>
   );
 }
 
