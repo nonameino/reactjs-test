0b1e48e2ab322816e5929da70a6e62233b35c463
Hung Nguyen Huy <hungnh1812@gmail.com>
2020-10-12 21:48:51 +0700

    Updates

---
+++ precache-manifest.79086a2dadbf49623302972402feb3cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c2cd7a0b97702fb31a87443eb3c330a",
+    "url": "/reactjs-test/index.html"
+  },
+  {
+    "revision": "fec2d7062f44bea5298d",
+    "url": "/reactjs-test/static/css/main.fd9e75a4.chunk.css"
+  },
+  {
+    "revision": "8cd519d3b587b88fd4c5",
+    "url": "/reactjs-test/static/js/2.beed5458.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactjs-test/static/js/2.beed5458.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fec2d7062f44bea5298d",
+    "url": "/reactjs-test/static/js/main.05365f3f.chunk.js"
+  },
+  {
+    "revision": "65fbf94b7091158d90b2",
+    "url": "/reactjs-test/static/js/runtime-main.6da9f528.js"
+  },
+  {
+    "revision": "2178fb0e1524e166850e558b75a0ee88",
+    "url": "/reactjs-test/static/media/Decorator.2178fb0e.png"
+  }
+]);
