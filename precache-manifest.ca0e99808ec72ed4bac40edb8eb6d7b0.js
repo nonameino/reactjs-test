e87f91d6f66000706c5ef7fdbadc797eec4b645d
Hung Nguyen Huy <hungnh1812@gmail.com>
2020-10-13 22:41:07 +0700

    Updates

--- precache-manifest.ca0e99808ec72ed4bac40edb8eb6d7b0.js
+++ precache-manifest.ca0e99808ec72ed4bac40edb8eb6d7b0.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4467035167f7c0404c54ed6f86234aa3",
+    "revision": "d5e8fb8bed53b6a82d3dac55e54d668b",
     "url": "/reactjs-test/index.html"
   },
   {
-    "revision": "3ce1a45e6d90b3b7e719",
+    "revision": "c64d2dda98b6f062604b",
     "url": "/reactjs-test/static/css/2.e452db76.chunk.css"
   },
   {
-    "revision": "80b8234e98ee3c217a0f",
-    "url": "/reactjs-test/static/css/main.6a9f3708.chunk.css"
+    "revision": "c2a2c1970476928c90e7",
+    "url": "/reactjs-test/static/css/main.f20a24fc.chunk.css"
   },
   {
-    "revision": "3ce1a45e6d90b3b7e719",
-    "url": "/reactjs-test/static/js/2.139e7447.chunk.js"
+    "revision": "c64d2dda98b6f062604b",
+    "url": "/reactjs-test/static/js/2.3de1aa85.chunk.js"
   },
   {
     "revision": "117d65892edc8adcd29d4a0d57b5a80a",
-    "url": "/reactjs-test/static/js/2.139e7447.chunk.js.LICENSE.txt"
+    "url": "/reactjs-test/static/js/2.3de1aa85.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "80b8234e98ee3c217a0f",
-    "url": "/reactjs-test/static/js/main.345f9d41.chunk.js"
+    "revision": "c2a2c1970476928c90e7",
+    "url": "/reactjs-test/static/js/main.5cc4f4ff.chunk.js"
   },
   {
     "revision": "65fbf94b7091158d90b2",
